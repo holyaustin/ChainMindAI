c5162f1a447806a9da980e2e444a50a4140785c4
holyaustin <holyaustin@yahoo.com>
2025-02-08 16:59:04 +0100

    hero component

--- client/components/hero-home.tsx
+++ client/components/hero-home.tsx
@@ -13,7 +13,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              AI-driven tools for product teams
+              AI-Powered Web3 Insight, Redefined.
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -21,8 +21,7 @@ export default function HeroHome() {
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
+                ChainindAI is revolutionizing the intersection of artificial intelligence and blockchain, delivering secure, transparent, and decentralized AI solutions for the future of Web3.
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
@@ -31,7 +30,7 @@ export default function HeroHome() {
                     href="#0"
                   >
                     <span className="relative inline-flex items-center">
-                      Start Building
+                      Get Started
                       <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
                         -&gt;
                       </span>
--- client/components/ui/footer.tsx
+++ client/components/ui/footer.tsx
@@ -214,7 +214,7 @@ export default function Footer() {
             </div>
             <div className="text-sm">
               <p className="mb-3 text-indigo-200/65">
-                © Cruip.com
+                © ChainMind-AI.vercel.com
                 <span className="text-gray-700"> · </span>
                 <a
                   className="text-indigo-200/65 transition hover:text-indigo-500"
